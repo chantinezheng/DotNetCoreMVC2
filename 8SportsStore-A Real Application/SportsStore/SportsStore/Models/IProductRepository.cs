e05a1cb2b53cbee45f3cf0934dd8270fcdddf9dd
chantinezheng <chantinezheng@hotmail.com>
2020-04-09 23:44:43 +0800

    add database

--- 8SportsStore-A Real Application/SportsStore/SportsStore/Models/IProductRepository.cs
+++ 8SportsStore-A Real Application/SportsStore/SportsStore/Models/IProductRepository.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace SportsStore.Models
 {
-    public class IProductRepository
+    public interface IProductRepository
     {
         IQueryable<Product> Products { get; }
     }
--- 8SportsStore-A Real Application/SportsStore/SportsStore/Startup.cs
+++ 8SportsStore-A Real Application/SportsStore/SportsStore/Startup.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SportsStore.Models;
@@ -13,11 +10,20 @@ namespace SportsStore
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
+        public IConfiguration Configuration { get; }
+     
+            
+            
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IProductRepository, FakeProductRepository>();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+                Configuration["Data:SportStoreProducts:ConnectionString"]));
+            //services.AddTransient<IProductRepository, FakeProductRepository>();
+            services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
@@ -42,8 +48,9 @@ namespace SportsStore
             app.UseStaticFiles();
             app.UseMvc(routes =>
             {
-
+                routes.MapRoute(name: "default", template: "{controller=Product}/{action=List}/{id?}");
             });
+            SeedData.EnsurePopulated(app);
         }
     }
 }
